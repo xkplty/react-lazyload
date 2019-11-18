5087f0fa2b183acdae24f325914dd75d18c47d58
pengli <pengli@kuaishou.com>
2019-11-18 12:17:41 +0800

    add sourcemap

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,20 @@
 import typescript from 'rollup-plugin-typescript'
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs';
+import { uglify } from "rollup-plugin-uglify";
 
 export default {
   input: './src/index.tsx',
   output: {
     file: './lib/react-lazyload.js',
-    format: 'cjs'
+    format: 'cjs',
+    sourcemap: true
   },
   external: [ 'react', 'react-dom' ],
   plugins: [
     typescript(),
     resolve(),
-    commonjs()
+    commonjs(),
+    uglify()
   ]
 }
